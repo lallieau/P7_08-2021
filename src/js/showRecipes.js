7cb0e432b1a83fced6080c90872ba90bbf300eeb
Lallie <lallie.audry@gmail.com>
2021-08-24 17:28:29 +0200

    filter recipe with param url

--- src/js/showRecipes.js
+++ src/js/showRecipes.js
@@ -1,64 +1,12 @@
 import { recipes } from "./recipes.js";
+import { recipesListTemplate } from "./templates/recipesListTemplate.js";
 
 const recipesList = document.querySelector("#list-recipes");
+const searchInput = document.querySelector(".form-control");
+const searchForm = document.querySelector(".form-inline");
 
-const recipeIngredientsListTemplate = (recipeIngredients) => {
-  if (!recipeIngredients.unit && !recipeIngredients.quantity) {
-    return `<li class="card-recipe__list-ingredients__name">
-        <strong>${recipeIngredients.ingredient}</strong>
-      </li>`;
-  }
-  if (!recipeIngredients.unit && recipeIngredients.quantity) {
-    return `<li class="card-recipe__list-ingredients__name">
-    <strong>${recipeIngredients.ingredient}:</strong> ${recipeIngredients.quantity}
-  </li>`;
-  }
-  return `<li class="card-recipe__list-ingredients__name">
-      <strong>${recipeIngredients.ingredient}:</strong> ${recipeIngredients.quantity} ${recipeIngredients.unit}
-    </li>`;
-};
-
-const recipesListTemplate = (recipe) => {
-  return `
-    <div class="card-recipe">
-        <img src="./src/public/assets/food.png" class="card-img-top" alt="..." />
-        <div class="card-recipe__body">
-            <div class="card-recipe__header">
-                <h2 class="card-recipe__title">${recipe.name}</h5>
-                <div class="card-recipe__time">
-                    <!-- <i class="bi bi-clock"></i> -->
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-clock"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                        d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
-                        />
-                        <path
-                        d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
-                        />
-                    </svg>
-                    <p>${recipe.time} min</p>
-                </div>
-            </div>
-            <div class="card-recipe__content">
-            <ul id="card-recipe__list-ingredients">
-            ${recipe.ingredients.map(recipeIngredientsListTemplate).join("")}
-            </ul>
-                <p class="card-recipe__text">
-                ${recipe.description}
-                </p>
-            </div>
-        </div>
-    </div>
-      `;
-};
-
-const searchValue = document.querySelector(".form-control");
+const searchParams = new URLSearchParams(window.location.search);
+const searchFilter = searchParams.get("searchBy");
 
 // const showRecipes = (searchValue) => {
 //   const filterRecipes = recipes.filter(
@@ -95,7 +43,8 @@ const searchValue = document.querySelector(".form-control");
 //   ]);
 // };
 
-const filterRecipesBySearchText = (searchText) => {
+const filterRecipesBySearchText = (searchFilter) => {
+  console.log(searchParams.get("searchBy"));
   const checkIfMatch = (text, search) => {
     const [formattedText, formattedSearch] = [
       text.trim().toLowerCase(),
@@ -122,7 +71,7 @@ const filterRecipesBySearchText = (searchText) => {
       name,
       description,
       ...ingredients.map(({ ingredient }) => ingredient),
-    ].some((value) => checkIfMatch(value, searchText))
+    ].some((value) => checkIfMatch(value, searchFilter))
   );
 };
 
@@ -138,11 +87,22 @@ const renderRecipes = () => {
     .join("");
 };
 
-searchValue.addEventListener("input", (e) => {
-  if (e.target.value.length >= 3) {
-    recipeFound = filterRecipesBySearchText(e.target.value);
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (searchInput.value.length >= 3) {
+    searchParams.append("searchBy", searchInput.value);
+
+    recipeFound = filterRecipesBySearchText(searchFilter);
     return renderRecipes();
   }
+});
+
+searchInput.addEventListener("change", () => {
+  searchParams.set("searchBy", searchInput.value);
+  window.location.search = searchParams;
+
+  recipeFound = filterRecipesBySearchText(searchFilter);
+  return renderRecipes();
 });
 
 renderRecipes();
---
+++ src/js/templates/recipesListTemplate.js
@@ -0,0 +1,55 @@
+const recipeIngredientsListTemplate = (recipeIngredients) => {
+  if (!recipeIngredients.unit && !recipeIngredients.quantity) {
+    return `<li class="card-recipe__list-ingredients__name">
+          <strong>${recipeIngredients.ingredient}</strong>
+        </li>`;
+  }
+  if (!recipeIngredients.unit && recipeIngredients.quantity) {
+    return `<li class="card-recipe__list-ingredients__name">
+      <strong>${recipeIngredients.ingredient}:</strong> ${recipeIngredients.quantity}
+    </li>`;
+  }
+  return `<li class="card-recipe__list-ingredients__name">
+        <strong>${recipeIngredients.ingredient}:</strong> ${recipeIngredients.quantity} ${recipeIngredients.unit}
+      </li>`;
+};
+
+export const recipesListTemplate = (recipe) => {
+  return `
+      <div class="card-recipe">
+          <img src="./src/public/assets/food.png" class="card-img-top" alt="..." />
+          <div class="card-recipe__body">
+              <div class="card-recipe__header">
+                  <h2 class="card-recipe__title">${recipe.name}</h5>
+                  <div class="card-recipe__time">
+                      <!-- <i class="bi bi-clock"></i> -->
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-clock"
+                          viewBox="0 0 16 16"
+                      >
+                          <path
+                          d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"
+                          />
+                          <path
+                          d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"
+                          />
+                      </svg>
+                      <p>${recipe.time} min</p>
+                  </div>
+              </div>
+              <div class="card-recipe__content">
+              <ul id="card-recipe__list-ingredients">
+              ${recipe.ingredients.map(recipeIngredientsListTemplate).join("")}
+              </ul>
+                  <p class="card-recipe__text">
+                  ${recipe.description}
+                  </p>
+              </div>
+          </div>
+      </div>
+        `;
+};
