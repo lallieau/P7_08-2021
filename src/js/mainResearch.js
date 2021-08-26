9684cf011b3b64407f8722fad4ffc44a3a9323f0
Lallie <lallie.audry@gmail.com>
2021-08-26 14:30:41 +0200

    refactoring

---
+++ src/js/mainResearch.js
@@ -0,0 +1,45 @@
+import { recipes } from "./recipes.js";
+
+const searchInput = document.querySelector(".form-control");
+const searchForm = document.querySelector(".form-inline");
+
+const searchParams = new URLSearchParams(window.location.search);
+searchParams.append("searchBy", "");
+export let searchFilter = searchParams.get("searchBy");
+
+export const filterRecipesBySearchText = (searchFilter) => {
+  const checkIfMatch = (text, search) => {
+    const [formattedText, formattedSearch] = [
+      text.trim().toLowerCase(),
+      search.trim().toLowerCase(),
+    ];
+    return formattedText.includes(formattedSearch);
+  };
+
+  return recipes.filter(({ name, description, ingredients }) =>
+    [
+      name,
+      description,
+      ...ingredients.map(({ ingredient }) => ingredient),
+    ].some((value) => checkIfMatch(value, searchFilter))
+  );
+};
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (searchInput.value.length >= 3) {
+    searchParams.set("searchBy", searchInput.value);
+
+    searchFilter = searchParams.get("searchBy");
+    recipeFound = filterRecipesBySearchText(searchFilter);
+    return renderRecipes();
+  }
+});
+
+searchInput.addEventListener("change", () => {
+  searchParams.set("searchBy", searchInput.value);
+  window.location.search = searchParams;
+
+  recipeFound = filterRecipesBySearchText(searchFilter);
+  return renderRecipes();
+});
--- src/js/showRecipes.js
+++ src/js/showRecipes.js
@@ -1,34 +1,9 @@
-import { recipes } from "./recipes.js";
 import { recipesListTemplate } from "./templates/recipesListTemplate.js";
 import { noResultTemplate } from "./templates/recipesListTemplate.js";
+import { filterRecipesBySearchText, searchFilter } from "./mainResearch.js";
 
 const recipesList = document.querySelector("#list-recipes");
 const noResult = document.querySelector(".no-result");
-const searchInput = document.querySelector(".form-control");
-const searchForm = document.querySelector(".form-inline");
-
-const searchParams = new URLSearchParams(window.location.search);
-searchParams.append("searchBy", "");
-let searchFilter = searchParams.get("searchBy");
-
-const filterRecipesBySearchText = (searchFilter) => {
-  const checkIfMatch = (text, search) => {
-    const [formattedText, formattedSearch] = [
-      text.trim().toLowerCase(),
-      search.trim().toLowerCase(),
-    ];
-
-    return formattedText.includes(formattedSearch);
-  };
-
-  return recipes.filter(({ name, description, ingredients }) =>
-    [
-      name,
-      description,
-      ...ingredients.map(({ ingredient }) => ingredient),
-    ].some((value) => checkIfMatch(value, searchFilter))
-  );
-};
 
 const recipeFound = filterRecipesBySearchText(searchFilter);
 
@@ -39,24 +14,5 @@ const renderRecipes = () => {
     .map((recipe) => recipesListTemplate(recipe))
     .join("");
 };
-
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (searchInput.value.length >= 3) {
-    searchParams.set("searchBy", searchInput.value);
-
-    searchFilter = searchParams.get("searchBy");
-    recipeFound = filterRecipesBySearchText(searchFilter);
-    return renderRecipes();
-  }
-});
-
-searchInput.addEventListener("change", () => {
-  searchParams.set("searchBy", searchInput.value);
-  window.location.search = searchParams;
-
-  recipeFound = filterRecipesBySearchText(searchFilter);
-  return renderRecipes();
-});
 
 renderRecipes();
