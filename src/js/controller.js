2f8d62aeaecc0da1f7562b52b8477034da219315
Lallie <lallie.audry@gmail.com>
2021-09-21 18:11:26 +0200

    debug filter by tags

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,40 +1,31 @@
 import { recipes } from "./data/recipes.js";
 
 export const filterRecipes = (searchQuery, filters) => {
-  let recipeFound = recipes;
-
-  const checkSearchQuery = (text, search) => {
-    const [formattedText, formattedSearch] = [
-      text.trim().toLowerCase(),
-      search.trim().toLowerCase(),
-    ];
-
-    if (formattedText.includes(formattedSearch)) {
-      return formattedText;
-    }
-  };
-
-  const checkIfFilterMatch = (filter, recipeTags) =>
-    recipeTags.some((recipeTag) => filter === recipeTag.toLowerCase().trim());
-
-  recipeFound = recipeFound.filter(({ name, description, ingredients }) =>
-    [
+  const checkSearchQuery = ({ name, description, ingredients }) => {
+    return [
       name,
       description,
       ...ingredients.map(({ ingredient }) => ingredient),
-    ].some((value) => checkSearchQuery(value, searchQuery))
-  );
+    ].some((text) => {
+      const [formattedText, formattedSearch] = [
+        text.trim().toLowerCase(),
+        searchQuery.trim().toLowerCase(),
+      ];
 
-  recipeFound = recipeFound.filter(({ ustensils, ingredients, appliance }) => {
-    const recipeTags = [
-      ...ustensils,
-      ...ingredients.map(({ ingredient }) => ingredient),
-      appliance,
-    ];
-    return filters.every((filter) =>
-      checkIfFilterMatch(filter.tag, recipeTags)
+      return formattedText.includes(formattedSearch);
+    });
+  };
+
+  const checkIfFilterMatch = ({ ustensils, ingredients, appliance }) =>
+    filters.every((filter) =>
+      [
+        ...ustensils,
+        ...ingredients.map(({ ingredient }) => ingredient),
+        appliance,
+      ].some((recipeTag) => filter.tag === recipeTag.toLowerCase().trim())
     );
-  });
 
-  return recipeFound;
+  return recipes.filter(
+    (recipe) => checkIfFilterMatch(recipe) && checkSearchQuery(recipe)
+  );
 };
