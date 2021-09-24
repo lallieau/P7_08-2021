4164fbecabb17ab9ba0a0261c1ed9e90f38b8a00
Lallie <lallie.audry@gmail.com>
2021-09-24 14:16:11 +0200

    code improvement

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,36 +1,31 @@
 import { recipes } from "./data/recipes.js";
 
 export const filterRecipes = (searchQuery, filters) => {
-  let recipeFound = recipes;
   const newRecipeFound = [];
 
   const checkIfFilterMatch = (filter, recipeTags) =>
     recipeTags.some((recipeTag) => filter === recipeTag.toLowerCase().trim());
 
-  const removeDuplicateRecipes = (array) =>
-    array.filter((item, index) => array.indexOf(item) === index);
+  for (let i = 0; i < recipes.length; i++) {
+    if (newRecipeFound[i]) continue;
 
-  for (let i = 0; i < recipeFound.length; i++) {
     if (
-      recipeFound[i].name.toLowerCase().includes(searchQuery) ||
-      recipeFound[i].description.toLowerCase().includes(searchQuery)
+      recipes[i].name.toLowerCase().includes(searchQuery) ||
+      recipes[i].description.toLowerCase().includes(searchQuery)
     ) {
-      newRecipeFound.push(recipeFound[i]);
+      newRecipeFound[i] = recipes[i];
     }
 
-    for (let d = 0; d < recipeFound[i].ingredients.length; d++) {
+    for (let d = 0; d < recipes[i].ingredients.length; d++) {
       if (
-        recipeFound[i].ingredients[d].ingredient
-          .toLowerCase()
-          .includes(searchQuery)
+        recipes[i].ingredients[d].ingredient.toLowerCase().includes(searchQuery)
       ) {
-        newRecipeFound.push(recipeFound[i]);
+        newRecipeFound[i] = recipes[i];
       }
     }
   }
-  recipeFound = removeDuplicateRecipes(newRecipeFound);
 
-  recipeFound = recipeFound.filter(({ ustensils, ingredients, appliance }) => {
+  return newRecipeFound.filter(({ ustensils, ingredients, appliance }) => {
     const recipeTags = [
       ...ustensils,
       ...ingredients.map(({ ingredient }) => ingredient),
@@ -40,6 +35,5 @@ export const filterRecipes = (searchQuery, filters) => {
       checkIfFilterMatch(filter.tag, recipeTags)
     );
   });
-
-  return recipeFound;
+  return newRecipeFound;
 };
